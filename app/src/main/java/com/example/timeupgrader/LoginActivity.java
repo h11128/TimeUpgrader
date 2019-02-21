4677cd025754cdd32b5e6bfdd2b412e63166f8b0
skyfaqer <37690820+skyfaqer@users.noreply.github.com>
2019-02-21 00:15:07 -0500

    log for all the life cycle

--- app/src/main/java/com/example/timeupgrader/LoginActivity.java
+++ app/src/main/java/com/example/timeupgrader/LoginActivity.java
@@ -67,9 +67,27 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause() called!!!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume() called!!!");
+    }
+
+    @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "onStop() called!!!");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart() called!!!");
     }
 
     @Override
--- app/src/main/java/com/example/timeupgrader/MainActivity.java
+++ app/src/main/java/com/example/timeupgrader/MainActivity.java
@@ -80,9 +80,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause() called!!!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume() called!!!");
+    }
+
+    @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "onStop() called!!!");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart() called!!!");
     }
 
     @Override
