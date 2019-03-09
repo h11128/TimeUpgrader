2afe810b4bb0d4d738b23bbd27e4f80584d781b1
Jason Yao <h11129@gmail.com>
2019-03-08 22:45:43 -0500

    fix

--- app/src/main/java/com/example/timeupgrader/TaskDatabaseHelper.java
+++ app/src/main/java/com/example/timeupgrader/TaskDatabaseHelper.java
@@ -18,68 +18,66 @@ public class TaskDatabaseHelper extends SQLiteOpenHelper {
     private static final int Version = 1;
 
 
-        public TaskDatabaseHelper(Context context){
-            super(context, DB_Name, null, Version);
-        }
+    public TaskDatabaseHelper(Context context){
+        super(context, DB_Name, null, Version);
+    }
 
-        @Override
-        public void OnCreate(SQLiteDatabase db) {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " +  UASchema.Table_UserAccount + " (" +
+                UASchema.Column_UserId + " TEXT PRIMARY KEY,"+ UASchema.Column_UserName +" TEXT," +
+                UASchema.Column_Email + " TEXT," + UASchema.Column_Password + " TEXT," +
+                UASchema.Column_Level+ " INTEGER," + UASchema.Column_Point + " INTEGER," +
+                UASchema.Column_NumFocuses + " INTEGER)");
 
+        db.execSQL("create table "+UserAchSchema.Table_UserAchievements +"(" +
+                UserAchSchema.Column_UAId +" INTEGER primary key autoincrement,"+
+                UASchema.Column_UserId +" TEXT,"+ AchSchema.Column_AchieveId +" INTEGER,"+
+                UserAchSchema.Column_AchieveTime +" INTEGER)");
 
-            db.execSQL("create table " +  UASchema.Table_UserAccount + " (" +
-                    UASchema.Column_UserId + " TEXT PRIMARY KEY,"+ UASchema.Column_UserName +" TEXT," +
-                    UASchema.Column_Email + " TEXT," + UASchema.Column_Password + " TEXT," +
-                    UASchema.Column_Level+ " INTEGER," + UASchema.Column_Point + " INTEGER," +
-                    UASchema.Column_NumFocuses + " INTEGER)");
+        db.execSQL("create table "+ AchSchema.Table_Achievements +" (" +
+                AchSchema.Column_AchieveId +" INTEGER primary key autoincrement,"+
+                AchSchema.Column_AchieveName +" TEXT,"+ AchSchema.Column_AchieveDescription +" TEXT," +
+                AchSchema.Column_Criterion +" INTEGER,"+ AchSchema.Column_Threshold +" INTEGER)");
 
-            db.execSQL("create table "+UserAchSchema.Table_UserAchievements +"(" +
-                    UserAchSchema.Column_UAId +" INTEGER primary key autoincrement,"+
-                    UASchema.Column_UserId +" TEXT,"+ AchSchema.Column_AchieveId +" INTEGER,"+
-                    UserAchSchema.Column_AchieveTime +" INTEGER)");
+        db.execSQL("create table "+ UGA.Table_UserGroupActivity +" (" +
+                UGA.Column_UGAId +" INTEGER primary key autoincrement,"+
+                UASchema.Column_UserId +" TEXT,"+ ACT.Column_ActId +" TEXT," +
+                UGA.Column_MemberStatus +" INTEGER,"+ UGA.Column_gTotalTime +" INTEGER,"+
+                UGA.Column_gCurTime +" INTERGER)");
 
-            db.execSQL("create table "+ AchSchema.Table_Achievements +" (" +
-                    AchSchema.Column_AchieveId +" INTEGER primary key autoincrement,"+
-                    AchSchema.Column_AchieveName +" TEXT,"+ AchSchema.Column_AchieveDescription +" TEXT," +
-                    AchSchema.Column_Criterion +" INTEGER,"+ AchSchema.Column_Threshold +" INTEGER)");
+        db.execSQL("create table "+ ACT.Table_Activity + " (" +
+                ACT.Column_ActId +" INTEGER primary key autoincrement,"+
+                ACT.Column_ActName +" TEXT,"+ ACT.Column_ActDescription +" TEXT," +
+                ACT.Column_ActType +" INTEGER,"+ ACT.Column_StartTime +" INTEGER,"+
+                ACT.Column_Notify +" INTEGER," + ACT.Column_IsTiming +" INTEGER,"+
+                ACT.Column_RewardPoint +" INTEGER,"+ ACT.Column_Status +" INTEGER)");
+    }
 
-            db.execSQL("create table "+ UGA.Table_UserGroupActivity +" (" +
-                    UGA.Column_UGAId +" INTEGER primary key autoincrement,"+
-                    UASchema.Column_UserId +" TEXT,"+ ACT.Column_ActId +" TEXT," +
-                    UGA.Column_MemberStatus +" INTEGER,"+ UGA.Column_gTotalTime +" INTEGER,"+
-                    UGA.Column_gCurTime +" INTERGER)");
-
-            db.execSQL("create table "+ ACT.Table_Activity + " (" +
-                    ACT.Column_ActId +" INTEGER primary key autoincrement,"+
-                    ACT.Column_ActName +" TEXT,"+ ACT.Column_ActDescription +" TEXT," +
-                    ACT.Column_ActType +" INTEGER,"+ ACT.Column_StartTime +" INTEGER,"+
-                    ACT.Column_Notify +" INTEGER," + ACT.Column_IsTiming +" INTEGER,"+
-                    ACT.Column_RewardPoint +" INTEGER,"+ ACT.Column_Status +" INTEGER)");
-
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     // Implement schema changes and data massage here when upgrading
-        }
-        public long insert_UserAccount(User user, Account account) {
+            
+    }
+    public long insert_UserAccount(Account account) {
             ContentValues cv = new ContentValues();
             cv.put(UASchema.Column_UserName, account.getUsername());
             cv.put(UASchema.Column_Password, account.getPassword());
             cv.put(UASchema.Column_UserId, account.getId());
             cv.put(UASchema.Column_Email,account.getEmail());
-            cv.put(UASchema.Column_Level, user.getLevel());
-            cv.put(UASchema.Column_Point, user.getPoint());
-            cv.put(UASchema.Column_NumFocuses, user.getNumFocusesDone());
+            //cv.put(UASchema.Column_Level, user.getLevel());
+            //cv.put(UASchema.Column_Point, user.getPoint());
+            //cv.put(UASchema.Column_NumFocuses, user.getNumFocusesDone());
             return getWritableDatabase().insert(UASchema.Table_UserAccount, null, cv);
         }
-        public long insert_UserAchievements(User user, Achievement achievement) {
+    public long insert_UserAchievements(User user, Achievement achievement) {
             ContentValues cv = new ContentValues();
             cv.put(UASchema.Column_UserId, user.getId());
             cv.put(AchSchema.Column_AchieveId, achievement.getId());
             cv.put(UserAchSchema.Column_AchieveTime, achievement.getThreshold());
             return getWritableDatabase().insert(UserAchSchema.Table_UserAchievements, null, cv);
         }
-        public long insert_Achievements(Achievement achievement) {
+    public long insert_Achievements(Achievement achievement) {
             ContentValues cv = new ContentValues();
             cv.put(AchSchema.Column_AchieveDescription, achievement.getDescription());
             cv.put(AchSchema.Column_AchieveName, achievement.getName());
@@ -87,7 +85,7 @@ public class TaskDatabaseHelper extends SQLiteOpenHelper {
             cv.put(AchSchema.Column_Threshold, achievement.getThreshold());
             return getWritableDatabase().insert(AchSchema.Table_Achievements, null, cv);
         }
-        public long insert_UserGroupActivity(GroupAct GA, Act act, User user){
+    public long insert_UserGroupActivity(GroupAct GA, Act act, User user){
             ContentValues cv = new ContentValues();
             cv.put(UGA.Column_gCurTime, GA.getCurrentTimeByUser(user));
             cv.put(UGA.Column_gTotalTime, GA.getTotalTimeByUser(user));
@@ -98,7 +96,7 @@ public class TaskDatabaseHelper extends SQLiteOpenHelper {
 
             return getWritableDatabase().insert(UGA.Table_UserGroupActivity, null, cv);
         }
-        public long insert_Activity(Act act){
+    public long insert_Activity(Act act){
             ContentValues cv = new ContentValues();
             cv.put(ACT.Column_ActId, act.getId());
             cv.put(ACT.Column_ActName, act.getName());
@@ -111,4 +109,11 @@ public class TaskDatabaseHelper extends SQLiteOpenHelper {
             //cv.put(ACT.Column_Status, act.getStatus());
             return getWritableDatabase().insert(ACT.Table_Activity, null, cv);
         }
-}
+    public int delete_UserAccount(Account account){
+            String selection = UASchema.Column_UserName + " LIKE ?";
+            String[] selectionArgs = { account.getUsername() };
+            int deletedRows = getWritableDatabase().delete(UASchema.Table_UserAccount, selection, selectionArgs);
+        };
+
+};
+
