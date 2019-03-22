00091e58f0c1429d6c20702474bdeb3946d440a9
skyfaqer <37690820+skyfaqer@users.noreply.github.com>
2019-03-22 19:52:31 -0400

    ...

--- app/src/main/java/com/example/timeupgrader/MainActivity.java
+++ app/src/main/java/com/example/timeupgrader/MainActivity.java
@@ -1,26 +1,15 @@
 package com.example.timeupgrader;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-    Toolbar toolbar;
-    ProgressBar progressBar;
-    Button exit;
-    Button signout;
-    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,42 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate() called!!!");
 
-        toolbar = findViewById(R.id.toolbarProfile);
-        progressBar = findViewById(R.id.progressBarProfile);
-        signout = findViewById(R.id.btnSignout);
-        exit = findViewById(R.id.btnExit);
-        fab = findViewById(R.id.addactivity);
-
-        toolbar.setTitle("TimeUpgrader");
-
-        exit.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                showNormalDialog();
-            }
-        });
-
-        signout.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sp = getSharedPreferences("login", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sp.edit();
-                editor.clear();
-                editor.apply();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                finish();
-            }
-        });
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ViewActivity.class));
-                finish();
-            }
-        });
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.container_main);
+        if (fragment == null) {
+            fragment = new MainFragment();
+            fm.beginTransaction().add(R.id.container_main, fragment).commit();
+        }
     }
 
     @Override
@@ -106,26 +65,5 @@ public class MainActivity extends AppCompatActivity {
     public static void sendFinishActivityBroadcast(Context context) {
         context.sendBroadcast(new Intent(SignupActivity.RECEIVER_ACTION_FINISH));
         context.sendBroadcast(new Intent(LoginActivity.RECEIVER_ACTION_FINISH));
-    }
-
-    private void showNormalDialog(){
-        final AlertDialog.Builder normalDialog =
-                new AlertDialog.Builder(MainActivity.this);
-        normalDialog.setTitle("Exit");
-        normalDialog.setMessage("Are you sure to exit?");
-        normalDialog.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        MainActivity.this.finish();
-                    }
-                });
-        normalDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        normalDialog.create().show();
     }
 }
--- app/src/main/java/com/example/timeupgrader/MainFragment.java
+++ app/src/main/java/com/example/timeupgrader/MainFragment.java
@@ -1,29 +1,101 @@
 package com.example.timeupgrader;
 
-
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class MainFragment extends Fragment {
+    Toolbar toolbar;
+    ProgressBar progressBar;
+    Button exit;
+    Button signout;
+    FloatingActionButton fab;
 
-
-    public MainFragment() {
-        // Required empty public constructor
-    }
-
+    public MainFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        toolbar = getActivity().findViewById(R.id.toolbarProfile);
+        progressBar = getActivity().findViewById(R.id.progressBarProfile);
+        signout = getActivity().findViewById(R.id.btnSignout);
+        exit = getActivity().findViewById(R.id.btnExit);
+        fab = getActivity().findViewById(R.id.addactivity);
+
+        toolbar.setTitle("TimeUpgrader");
+
+        exit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showNormalDialog();
+            }
+        });
+
+        signout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sp = getActivity().getSharedPreferences("login", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                getActivity().finish();
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), ViewActivity.class));
+            }
+        });
+    }
+
+    private void showNormalDialog(){
+        final AlertDialog.Builder normalDialog =
+                new AlertDialog.Builder(getActivity());
+        normalDialog.setTitle("Exit");
+        normalDialog.setMessage("Are you sure to exit?");
+        normalDialog.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                getActivity().finish();
+            }
+        });
+        normalDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        normalDialog.create().show();
+    }
 }
