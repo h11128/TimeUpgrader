8c1abfdc309d591804faf2b8c9fd530b667f6350
skyfaqer <37690820+skyfaqer@users.noreply.github.com>
2019-03-23 22:18:28 -0400

    ...

--- app/src/main/java/com/example/timeupgrader/FocusFragment.java
+++ app/src/main/java/com/example/timeupgrader/FocusFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
 
 public class FocusFragment extends Fragment {
 
@@ -14,7 +17,27 @@ public class FocusFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_focus, container, false);
+        View v = inflater.inflate(R.layout.fragment_focus, container, false);
+
+        NumberPicker hr = v.findViewById(R.id.focusHr);
+        NumberPicker min = v.findViewById(R.id.focusMin);
+        NumberPicker sec = v.findViewById(R.id.focusSec);
+
+        hr.setMaxValue(24);
+        hr.setMinValue(0);
+        hr.setValue(1);
+        min.setMaxValue(59);
+        min.setMinValue(0);
+        min.setValue(0);
+        sec.setMaxValue(59);
+        sec.setMinValue(0);
+        sec.setValue(0);
+
+        hr.setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
+        min.setDescendantFocusability(TimePicker.FOCUS_BLOCK_DESCENDANTS);
+        sec.setDescendantFocusability(TimePicker.FOCUS_BLOCK_DESCENDANTS);
+
+        return v;
     }
 
     @Override
