cda0c0dda8ae472c8aab8f9de8ab82ff3b1799f4
Jason Yao <h11129@gmail.com>
2019-03-27 12:07:02 -0400

    buttons

--- app/src/main/java/com/example/timeupgrader/MoreFragment.java
+++ app/src/main/java/com/example/timeupgrader/MoreFragment.java
@@ -1,24 +1,77 @@
 package com.example.timeupgrader;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MoreFragment extends Fragment {
-
+public class MoreFragment extends Fragment implements View.OnClickListener {
+    private Button mAccountButton;
+    private Button mSettingButton;
+    private Button mAchievementButton;
+    private Button mThemeButton;
+    private Button mHelpButton;
+    private Button mSyncButton;
     public MoreFragment() {}
-
+    View view;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        view =  inflater.inflate(R.layout.fragment_more, container, false);
+        mAccountButton =(Button) view.findViewById(R.id.btnAccountManagement);
+        mAccountButton.setOnClickListener(this);
+
+        mSettingButton =(Button) view.findViewById(R.id.btnSettings);
+        mSettingButton.setOnClickListener(this);
+
+        mAchievementButton =(Button) view.findViewById(R.id.btnAchievement);
+        mAchievementButton.setOnClickListener(this);
+
+        mThemeButton =(Button) view.findViewById(R.id.btnTheme);
+        mThemeButton.setOnClickListener(this);
+
+        mHelpButton =(Button) view.findViewById(R.id.btnHelp);
+        mHelpButton.setOnClickListener(this);
+
+        mSyncButton =(Button) view.findViewById(R.id.btnSync);
+        mSyncButton.setOnClickListener(this);
+        return view;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        onClick(view);
+
+
+
+
+    }
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.btnAccountManagement:
+                startActivity(new Intent(getActivity(), AccountActivity.class));
+                break;
+            case R.id.btnSettings:
+                startActivity(new Intent(getActivity(), SettingActivity.class));
+                break;
+            case R.id.btnAchievement:
+                startActivity(new Intent(getActivity(), AchievementActivity.class));
+                break;
+            case R.id.btnTheme:
+                //
+                break;
+            case R.id.btnHelp:
+                //
+                break;
+            case R.id.btnSync:
+                //
+                break;
+
+        }
     }
 }
