131e34246c8d245fdd5b1b68be3d8fd3690b7238
Yuanzhe Yang <yang.4766@osu.edu>
2019-03-28 23:47:14 -0400

    ...

--- app/src/main/java/com/example/timeupgrader/ViewActivity.java
+++ app/src/main/java/com/example/timeupgrader/ViewActivity.java
@@ -1,13 +1,14 @@
 package com.example.timeupgrader;
 
-import android.app.FragmentManager;
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,16 +20,20 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import org.joda.time.LocalDateTime;
 
-import java.util.Date;
+import java.text.DateFormat;
+import java.util.Calendar;
 
-public class ViewActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+public class ViewActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
 
     EditText editTextName;
     Button buttonAddTask;
     Button buttonPickTime;
+    Button buttonPickDate;
     Spinner spinnerType;
     TextView textView;
     DatabaseReference databaseTasks;
+    FireBaseHelper fbHelper;
+    TaskDatabaseHelper taskDatabaseHelper;
     private LocalDateTime mLocalDateTime = new LocalDateTime();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +44,9 @@ public class ViewActivity extends AppCompatActivity implements TimePickerDialog.
         buttonAddTask = (Button) findViewById(R.id.buttonAddTask);
         spinnerType = (Spinner) findViewById(R.id.spinnerType);
         buttonPickTime = (Button) findViewById(R.id.buttonPickTime);
+        buttonPickDate = (Button) findViewById(R.id.buttonPickDate);
         textView = (TextView)findViewById(R.id.textView);
+        textView = (TextView)findViewById(R.id.textView2);
         databaseTasks = FirebaseDatabase.getInstance().getReference();
         buttonPickTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,14 +55,34 @@ public class ViewActivity extends AppCompatActivity implements TimePickerDialog.
                 timePicker.show(getSupportFragmentManager(), "time picker ");
             }
         });
+        buttonPickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"date picker");
+            }
+        });
     }
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         TextView textView = (TextView)findViewById(R.id.textView);
-        textView.setText("Hour: " + hourOfDay + " Minute " + minute);
+        textView.setText(hourOfDay + "  :  " + minute );
+//        User u = User.getCurrentUser();
+//        SingleAct act = new SingleAct()；
+//        fbHelper.insertAct(act);
+//        taskDatabaseHelper.insert_Activity(act);
+    }
 
-
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        TextView textView2 = (TextView)findViewById(R.id.textView2);
+        textView2.setText(currentDateString);
     }
 
     private void addTask(){
