15d29072b0f7eef98086017c759f503ae757b26f
Yuanzhe Yang <yang.4766@osu.edu>
2019-04-12 21:35:48 -0400

    ..

--- app/src/androidTest/java/com/example/timeupgrader/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/timeupgrader/ExampleInstrumentedTest.java
@@ -2,12 +2,15 @@ package com.example.timeupgrader;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,7 +18,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+    public static final String STRING_TO_BE_TYPED = "Espresso";
+    @Rule
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
